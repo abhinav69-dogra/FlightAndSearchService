51aed87bcc87bf4879c3b474a52a1f6e7b8b30ed
Abhinav Dogra <65779864+abhinav69-dogra@users.noreply.github.com>
2024-02-19 11:24:53 +0530

    added seeded data

--- src/models/city.js
+++ src/models/city.js
@@ -11,8 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.airport, {
-        foreignKey:'cityId',
+      this.hasMany(models.Airport, {
+        foreignKey:'cityId'
       });
     }
   }
