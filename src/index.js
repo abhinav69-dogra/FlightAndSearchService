f267c02d346ac2b67c8f38487235c8b523f297d6
Abhinav Dogra <65779864+abhinav69-dogra@users.noreply.github.com>
2024-02-20 08:51:57 +0530

    added db sync

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const {PORT} = require('./config/serverConfig')
 const ApiRoutes = require('./routes/index');
+const db = require('./models/index')
 
 const setupAndStartServer = async () => {
 
@@ -13,6 +14,9 @@ const setupAndStartServer = async () => {
 
     app.listen(PORT, () => {
         console.log(`Server started at PORT ${PORT}`);
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter:true});
+        }
     })
 }
 
